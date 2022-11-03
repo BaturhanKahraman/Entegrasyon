ada8ef92fdae621523ed5bcf8c9e1845ca711fe0
Baturhan Kahraman <baturhan.kahraman@sigun.com.tr>
2022-11-03 19:52:10 +0300

    marketplace integration

---
+++ WorkerServices/TrendyolBackgroundService/TrendyolProductManager.cs
@@ -0,0 +1,41 @@
+﻿using Marketplace;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace TrendyolBackgroundJob
+{
+    public class TrendyolProductManager:IMarketPlaceProductService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<TrendyolProductManager> _logger;
+        public TrendyolProductManager(HttpClient client, ILogger<TrendyolProductManager> logger)
+        {
+            _httpClient = client;
+            _logger = logger;
+        }
+        public Task AddProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateStock()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WorkerServices/TrendyolBackgroundService/TrendyolSaleManager.cs
@@ -0,0 +1,28 @@
+﻿
+using Marketplace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendyolBackgroundJob
+{
+    public class TrendyolOrderManager : IMarketPlaceOrderService
+    {
+        public Task AbortOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CheckNewOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
