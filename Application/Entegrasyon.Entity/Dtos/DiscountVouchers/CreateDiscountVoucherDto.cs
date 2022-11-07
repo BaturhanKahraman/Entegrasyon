b634e043482d7ec5d7171136431e3662330a3ba2
Baturhan Kahraman <baturhan.kahraman@sigun.com.tr>
2022-11-07 14:00:14 +0300

    discount voucher merge

---
+++ Application/Entegrasyon.Entity/Dtos/DiscountVouchers/CreateDiscountVoucherDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Entegrasyon.Entity.Dtos.DiscountVouchers;
+
+public record CreateDiscountVoucherDto(decimal Amount, DateTimeOffset? ExpiringDay = null, int? CustomerId = null);
+
---
+++ Application/Entegrasyon.Entity/Dtos/DiscountVouchers/DiscountVoucherDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Entegrasyon.Entity.Dtos.DiscountVouchers;
+
+public record DiscountVoucherDto(
+    int Id,
+    string Code,
+    double Percentage,
+    decimal Amount,
+    DateTimeOffset? ExpiringDate,
+    string customerFullName,
+    string phoneNumber,
+    string IdentityNumber,
+    string TaxNumber
+    );
