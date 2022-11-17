f22b4c818a44dcb634c4d3bc398bbfadb5049765
Baturhan Kahraman <baturhan.kahraman@sigun.com.tr>
2022-11-17 15:29:41 +0300

    app life time added.

--- Application/Entegrasyon.API/Program.cs
+++ Application/Entegrasyon.API/Program.cs
@@ -1,25 +1,22 @@
 using MainDatabase.Context;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Shared.Extensions;
 using System.Text;
 using Entegrasyon.Business.Extensions;
 using Shared.FileStorage;
-using Shared.Middlewares;
 using Shared.Logger.Serilog;
+using Entegrasyon.Business.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior",true);
 builder.Services.AddControllers();
 builder.Services.AddLogging();
-builder.Services.Configure<ApiBehaviorOptions>(o=>o.SuppressModelStateInvalidFilter=true);
-builder.Services.Configure<Shared.Security.Jwt.TokenOptions>(builder.Configuration.GetSection("JwtTokenOptions"));
+builder.Services.AddConfigurations(builder.Configuration);
 builder.Services.AddApplicationDependencies();
 builder.Services.AddFileStorageCore();
 builder.Services.AddLocalFileStorage(x=>builder.Configuration.GetSection("LocalFileStorageOptions"));
-//builder.Services.AddLocalFileStorage(new LocalFileStorageOption{RootPath = "wwwroot"});
 
 builder.Services.AddDbContext<HeadDbContext>(x =>
 {
@@ -28,8 +25,6 @@ builder.Services.AddDbContext<HeadDbContext>(x =>
 builder.Services.AddCors(options =>
     options.AddPolicy("myclient",policy =>
         policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
-
-
 
 builder.Services.AddStackExchangeRedisCache(opt =>
     {
@@ -57,15 +52,16 @@ builder.Services.AddAuthorization();
 builder.Services.AddHttpClient();
 builder.AddSerilogWithLoggerProvider(builder.Configuration);
 var app = builder.Build();
-
-app.AddCustomExceptionHandlerMiddleware();
-// Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment())
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseMiddleware<MigrateDatabaseMiddleware>();
-}
+    var lifeTimeHandler = app.Services.GetService<ApplicationLifetimeManager>();
+    if(lifeTimeHandler != null)
+        await lifeTimeHandler.ApplyStartActions();
+});
+app.AddCustomExceptionHandlerMiddleware();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors("myclient");
 
---
+++ Application/Entegrasyon.Business/Concrete/ApplicationLifetimeManager.cs
@@ -0,0 +1,29 @@
+﻿using Entegrasyon.DataAccess.Concrete.EntityFrameworkCore.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Entegrasyon.Business.Concrete;
+
+public class ApplicationLifetimeManager
+{
+    private readonly IntegrationDbContext _context;
+    private readonly ILogger<ApplicationLifetimeManager> _logger;
+    public ApplicationLifetimeManager(IntegrationDbContext context, ILogger<ApplicationLifetimeManager> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task ApplyStartActions()
+    {
+        await MigrateDatabase();
+        _logger.LogInformation("Uygulama başlatıldı.");
+    }
+    private async Task MigrateDatabase(CancellationToken ct = default)
+    {
+        var pendingMigrations = await _context.Database.GetPendingMigrationsAsync(ct);
+        if (pendingMigrations.Any())
+            await _context.Database.MigrateAsync(ct);
+    }
+
+}
