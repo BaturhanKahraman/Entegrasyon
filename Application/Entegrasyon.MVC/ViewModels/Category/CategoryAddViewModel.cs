8e99c677f7a358ab03519aa30a1ed7b5784926f9
BaturhanKahraman <baturhan.kahraman@gmail.com>
2023-04-28 18:31:42 +0200

    Exclusive boolean attribute & Viewmodels

--- Application/Entegrasyon.MVC/ViewModels/Category/CategoryAddViewModel.cs
+++ Application/Entegrasyon.MVC/ViewModels/Category/CategoryAddViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace Entegrasyon.MVC.ViewModels.Category;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entegrasyon.MVC.ViewModels.Category;
 
 public class CategoryAddViewModel
 {
+    [Display(Name="Kategori İsmi")]
+    [StringLength(45,MinimumLength =2,ErrorMessage ="Lütfen en az 2 en fazla 45 karakter girin.")]
+    [Required(AllowEmptyStrings=false,ErrorMessage ="Bu alan gereklidir. Boşluklardan oluşamaz.")]
     public string Name { get; set; }
     public List<CategoryAttributeAddViewModel> CategoryAttributes { get; set; } = new();
     public int? SuperCategoryId { get; set; }
-    public bool IsFavorite { get; set; }
+    public bool IsFavorite { get; set; } = false;
 
 }
--- Application/Entegrasyon.MVC/ViewModels/Products/ProductDetailListViewModel.cs
+++ Application/Entegrasyon.MVC/ViewModels/Products/ProductDetailListViewModel.cs
@@ -1,16 +1,16 @@
 ﻿namespace Entegrasyon.MVC.ViewModels.Products
 {
-    public class ProductDetailListViewModel {
-
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string StockCode { get; set; }
-        public string BrandName { get; set; }
-        public string CategoryName { get; set; }
-        public int TotalQuantity { get; set; }
-        public int TotalSoldQuantity { get; set; }
+    public record ProductDetailListViewModel(
+        Guid Id,
+        string Title,
+        string Description,
+        string StockCode,
+        string BrandName,
+        string CategoryName,
+        int TotalQuantity,
+        int TotalSoldQuantity,
+        int VariantCount)
+    {
         public int TotalCurrentStock => TotalQuantity - TotalSoldQuantity;
-        public int VariantCount { get; set; }
-    } 
+    }
 }
