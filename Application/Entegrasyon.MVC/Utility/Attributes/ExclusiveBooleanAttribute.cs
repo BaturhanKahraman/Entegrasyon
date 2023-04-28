71040e12d067a7c15c8d9202f5e49bdc1627b006
BaturhanKahraman <baturhan.kahraman@gmail.com>
2023-04-28 18:31:42 +0200

    Exclusive boolean attribute & Viewmodels

---
+++ Application/Entegrasyon.MVC/Utility/Attributes/ExclusiveBooleanAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entegrasyon.MVC.Utility.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class ExclusiveBooleanAttribute : ValidationAttribute
+{
+    private readonly string _comparedBooleanProp;
+    public ExclusiveBooleanAttribute(string nameOfComparedBoolProp)
+    {
+        if(string.IsNullOrEmpty(nameOfComparedBoolProp))
+            ArgumentException.ThrowIfNullOrEmpty(nameof(nameOfComparedBoolProp));
+        _comparedBooleanProp = nameOfComparedBoolProp;
+    }
+    protected override ValidationResult IsValid(object value,ValidationContext validationContext)
+    {
+        if(value is not bool boolValue)
+            throw new InvalidOperationException("Bu attribute yalnızca boolean değişkenler için kullanılabilir.");
+        var otherPropertyInfo = validationContext.ObjectType.GetProperty(_comparedBooleanProp);
+        if(otherPropertyInfo == null)
+            throw new InvalidOperationException(_comparedBooleanProp + " bulunamadı");
+        bool? otherPropertyValue = otherPropertyInfo.GetValue(validationContext.ObjectInstance) as bool?;
+        if(!otherPropertyValue.HasValue ||
+            (otherPropertyValue == false && boolValue == false) || 
+            otherPropertyValue != boolValue)
+            return ValidationResult.Success;
+
+        return new ValidationResult($"{_comparedBooleanProp} değeri ve {validationContext.MemberName} özelliklerinin ikisi de aynı anda açık olamaz.");
+    }
+}
--- Application/Entegrasyon.MVC/ViewModels/Category/CategoryAttributeAddViewModel.cs
+++ Application/Entegrasyon.MVC/ViewModels/Category/CategoryAttributeAddViewModel.cs
@@ -1,17 +1,27 @@
-﻿using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
 using Entegrasyon.Entity.Categories;
+using Entegrasyon.MVC.Utility.Attributes;
 
 namespace Entegrasyon.MVC.ViewModels.Category;
 
 public class CategoryAttributeAddViewModel
 {
-    public int Id { get; set; }
-    public bool IsRequired { get; set; }
+    public int? Id { get; set; }
+    [Display(Name ="Zorunlu mu?",Description ="Bu kategoriden bir ürün oluştururken zorunlu olan değer.")]
+    public bool IsRequired { get; set; } = false;
+    [Display(Name ="Düz yazıya izin ver.",Description ="Bu seçenek işaretlendiğinde, değer yazılarak girilir.")]
     public bool AllowCustom { get; set; }
-    public bool IsVarianter { get; set; }
+    [ExclusiveBoolean(nameof(IsSlicer),ErrorMessage = "İki özellik de aynı anda aktif olamaz.")]
+    [Display(Name ="Varyant")]
+    public bool IsVarianter { get; set; } = false;
+    [Display(Name ="Sayfa Bölücü")]
+    public bool IsSlicer { get; set; } = false;
+    [Required(AllowEmptyStrings =false,ErrorMessage ="Lütfen özellik için bir isim girin.")]
+    [MaxLength(55,ErrorMessage ="Özellik ismi 55 karakterden fazla olamaz.")]
+    [MinLength(2,ErrorMessage ="Özellik ismi 2 karakterden az olamaz.")]
+    [Display(Name ="Kategori Özellik İsmi")]
     public string CategoryAttributeKey { get; set; }
-    public bool IsSlicer { get; set; }
-    public string CategoryAttributeHumanized { get; set; }
-    public string CategoryAttributeValues { get; set; }
-    //public List<CategoryAttributeValue> CategoryAttributeValues { get; set; }
+    public List<CategoryAttributeValueViewModel> CategoryAttributeValues { get; set; } = new();
+
 }
