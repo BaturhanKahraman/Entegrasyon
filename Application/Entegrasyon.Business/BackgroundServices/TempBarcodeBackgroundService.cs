c7b362e44876149b849dff0672a6413646b02012
baturhan kahreman <baturhan.kahraman@gmail.com>
2023-10-19 09:22:35 +0200

    Session menu tracker added.

--- Application/Entegrasyon.Business/BackgroundServices/TempBarcodeBackgroundService.cs
+++ Application/Entegrasyon.Business/BackgroundServices/TempBarcodeBackgroundService.cs
@@ -6,23 +6,23 @@ using Microsoft.Extensions.Logging;
 
 namespace Entegrasyon.Business.BackgroundServices;
 
-public class TempBarcodeBackgroundService:BackgroundService
+public class TempBarcodeBackgroundService : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
     private readonly IConfiguration _configuration;
     private readonly ILogger<TempBarcodeBackgroundService> _logger;
 
-    public TempBarcodeBackgroundService( IServiceProvider serviceProvider,IConfiguration configuration, ILogger<TempBarcodeBackgroundService> logger)
+    public TempBarcodeBackgroundService(IServiceProvider serviceProvider,IConfiguration configuration,ILogger<TempBarcodeBackgroundService> logger)
     {
         _serviceProvider = serviceProvider;
         _configuration = configuration;
         _logger = logger;
     }
-    
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        int delayInMinute = _configuration.GetValue("Barcode:ClearDelay", 10);
-        while (!stoppingToken.IsCancellationRequested)
+        int delayInMinute = _configuration.GetValue("Barcode:ClearDelay",10);
+        while(!stoppingToken.IsCancellationRequested)
         {
             await Task.Delay(TimeSpan.FromMinutes(delayInMinute),stoppingToken);
             //await Task.Delay(TimeSpan.FromSeconds(10),stoppingToken);
@@ -33,9 +33,9 @@ public class TempBarcodeBackgroundService:BackgroundService
                 await barcodeManager.ClearAddedBarcodes();
                 await barcodeManager.CorrectAddables();
             }
-            catch (Exception p)
+            catch(Exception p)
             {
-               _logger.LogCritical("Temp barkod hata fırlattı",p);
+                _logger.LogCritical("Temp barkod hata fırlattı " + p.Message,p);
             }
         }
     }
---
+++ Application/Entegrasyon.MVC/Utility/Storage/MenuStorage.cs
@@ -0,0 +1,18 @@
+﻿using Entegrasyon.MVC.Utility.Objects;
+
+namespace Entegrasyon.MVC.Utility.Storage;
+
+public static class MenuStorage
+{
+    public static Dictionary<string,NavigationItem> Menus = new()
+    {
+        ["product"] = new NavigationItem("Ürünler","Products"),
+        ["sale"] = new NavigationItem("Satış","Sales"),
+        ["branchOffice"] = new NavigationItem("Depo/Ofis","Offices"),
+        ["category"] = new NavigationItem("Kategori","Categories"),
+        ["customer"] = new NavigationItem("Müşteriler","Customers"),
+        ["order"] = new NavigationItem("Siparişler","Orders"),
+        ["user"] = new NavigationItem("Kullanıcılar","Users"),
+        ["cargo"] = new NavigationItem("Cargo","Cargos")
+    };
+}
