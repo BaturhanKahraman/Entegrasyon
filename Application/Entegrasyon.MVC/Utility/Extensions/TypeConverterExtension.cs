c26b3547dce41c692c6c823d2428a97c9ec563e1
BaturhanK <b.kahraman@ucas.ac.uk>
2023-12-10 03:03:35 +0000

    postgre hatası

--- Application/Entegrasyon.MVC/Utility/Extensions/TypeConverterExtension.cs
+++ Application/Entegrasyon.MVC/Utility/Extensions/TypeConverterExtension.cs
@@ -1,5 +1,7 @@
-﻿using Entegrasyon.Entity.Sales;
+﻿using Entegrasyon.Entity.Dtos.Category.Import.TrendyolImport;
+using Entegrasyon.Entity.Sales;
 using Entegrasyon.MVC.ViewModels.CategoryAttribute;
+using Entegrasyon.MVC.ViewModels.CategoryImport;
 using System.Text.Json;
 
 namespace Entegrasyon.MVC.Utility.Extensions;
@@ -10,5 +12,14 @@ public static class TypeConverterExtension
     public static TagifyValue ToTagifyValue(this CategoryAttributeValueViewModel vm)
         => new TagifyValue(vm.Id ?? 0, vm.Name);
     public static List<TagifyValue> ToTagifyValue(this List<CategoryAttributeValueViewModel> vm)
-       => vm.Select(v=>new TagifyValue(v.Id ?? 0, v.Name)).ToList();
+       => vm.Select(v=>v?.ToTagifyValue()).ToList();
+    public static JsTreeViewModel ToJsTree(this ImportedTrendyolCategory cat)
+        => new()
+        {
+            Id=cat.Id.ToString(),
+            Text=cat.Name,
+            Children=cat.SubCategories.Select(sc=>sc.ToJsTree()).ToArray()
+        };
+    public static IEnumerable<JsTreeViewModel> ToJsTreeList(this IEnumerable<ImportedTrendyolCategory> catList)
+        => catList.Select(c => c?.ToJsTree());
 }
