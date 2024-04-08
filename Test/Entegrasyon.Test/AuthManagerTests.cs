d3df4c1ba02fa459d74a4de501e5fafc5325c3a8
baturhan kahreman <baturhan.kahraman@gmail.com>
2024-04-08 16:23:29 +0200

    auth manager invalid password test added. IAuthServiceAdded, Authmanager renamed as AuthService

--- Test/Entegrasyon.Test/AuthManagerTests.cs
+++ Test/Entegrasyon.Test/AuthManagerTests.cs
@@ -11,11 +11,11 @@ namespace Entegrasyon.UnitTest
 {
     public class AuthManagerTests : BaseTest
     {
-        private readonly AuthManager authManager;
+        private readonly AuthService authManager;
 
         public AuthManagerTests()
         {
-            authManager = new AuthManager(integrationDbContextMock.Object,applicationLoggerMock.Object);
+            authManager = new AuthService(integrationDbContextMock.Object,applicationLoggerMock.Object);
         }
 
         //[Fact]
@@ -58,7 +58,33 @@ namespace Entegrasyon.UnitTest
                 Id = userId,
                 Name = "Test",
                 Surname = "User",
-                UserName = userName+"1",//here
+                UserName = userName + "1",//here
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                NeedsTakeNewPassword = false,
+                Roles = new List<Role>()
+            };
+            IList<ApplicationUser> users = [user];
+            integrationDbContextMock.Setup(c => c.Users).ReturnsDbSet(users);
+            //Act
+            var result = await authManager.LoginAsync(userName,password);
+            //Assert
+            result.Should().BeOfType<ErrorResult>();
+        }
+        [Fact]
+        public async Task LoginAsync_WithInValidPassword_ReturnsErrorResult()
+        {
+            //Arrange
+            string userName = "test";
+            string password = "testPassword";
+            HashingHelper.CreatePasswordHash("thisiswrongpassword",out var passwordHash,out var passwordSalt);
+            Guid userId = Guid.NewGuid();
+            ApplicationUser user = new ApplicationUser()
+            {
+                Id = userId,
+                Name = "Test",
+                Surname = "User",
+                UserName = userName,
                 PasswordHash = passwordHash,
                 PasswordSalt = passwordSalt,
                 NeedsTakeNewPassword = false,
