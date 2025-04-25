fc4c2f4caa8057fdb77a08db11a6b7fcbc7e5c11
BaturhanKahraman <baturhan.kahraman@gmail.com>
2025-04-25 23:40:09 +0200

    mapster needs to be deleted.

---
+++ Application/Entegrasyon.MVC/Utility/Mapper/Mappings.cs
@@ -0,0 +1,34 @@
+﻿using Entegrasyon.Entity.Dtos.Product;
+using Entegrasyon.MVC.ViewModels.Products;
+using Shared.Entity;
+
+namespace Entegrasyon.MVC.Utility.Mapper;
+
+public static class Mappings
+{
+    public static Pageable<TDestination> MapItems<TSource, TDestination>(
+       this Pageable<TSource> source,
+       Func<TSource,TDestination> mapFunc)
+    {
+        return new Pageable<TDestination>(
+            [.. source.Items.Select(mapFunc)],
+            source.CurrentPageIndex + 1,
+            source.PagingItemCount,
+            source.TotalItemCount
+        );
+    }
+
+    public static Func<ProductsDetailDto,ProductDetailListViewModel> ToListViewModel =>
+    item => new ProductDetailListViewModel(
+            item.Id,
+            item.Title,
+            item.Description,
+            item.StockCode,
+            item.BrandName,
+            item.CategoryName,
+            item.TotalQuantity,
+            item.TotalSoldQuantity,
+            item.VariantCount
+        );
+
+}
